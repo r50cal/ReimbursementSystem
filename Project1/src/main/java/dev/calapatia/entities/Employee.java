b2130a48b29ad47e71c1f4aec6816d440c9bfaae
Raymond <raymond.50cal93@gmail.com>
2020-04-10 16:47:24 -0400

    Added DAO's and Services

---
+++ Project1/src/main/java/dev/calapatia/entities/Employee.java
@@ -0,0 +1,37 @@
+package dev.calapatia.entities;
+
+public class Employee {
+private int eId;
+private String username;
+private String password;
+public Employee(int eId, String username, String password) {
+	super();
+	this.eId = eId;
+	this.username = username;
+	this.password = password;
+}
+@Override
+public String toString() {
+	return "Employee [eId=" + eId + ", username=" + username + ", password=" + password + "]";
+}
+public int geteId() {
+	return eId;
+}
+public void seteId(int eId) {
+	this.eId = eId;
+}
+public String getUsername() {
+	return username;
+}
+public void setUsername(String username) {
+	this.username = username;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+
+
+}
