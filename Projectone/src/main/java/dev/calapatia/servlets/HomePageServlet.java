0b4bf540c2f42a75dadca7d06fe207c612cdbd29
Raymond <raymond.50cal93@gmail.com>
2020-04-26 13:22:27 -0400

    Added Servlets

---
+++ Projectone/src/main/java/dev/calapatia/servlets/HomePageServlet.java
@@ -0,0 +1,54 @@
+package dev.calapatia.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.google.gson.Gson;
+
+import dev.calapatia.Service.ReimbursementService;
+import dev.calapatia.Service.ReimbursementServiceImpl;
+import dev.calapatia.entities.Reimbursement;
+
+/**
+ * Servlet implementation class EmployeeReimbursementInfo
+ */
+public class HomePageServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HomePageServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    ReimbursementService rserv = new ReimbursementServiceImpl();
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username = (String) request.getSession().getAttribute("user");
+		response.getWriter().append(username + request.getSession().getId());
+
+        
+        
+ 
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
